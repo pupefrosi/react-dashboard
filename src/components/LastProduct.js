1dfebec5795aa27b001f0384b4b4484527128e64
Maria Guadalupe Rodriguez Frosi <109034529+pupefrosi@users.noreply.github.com>
2023-06-05 22:39:37 -0300

    Update LastProduct.js

--- src/components/LastProduct.js
+++ src/components/LastProduct.js
@@ -20,7 +20,7 @@ function LastProduct(){
         <div className="col-lg-6 mb-4">
             <div className="card shadow mb-4">
                 <div className="card-header py-3">
-                    <h5 className="m-0 font-weight-bold text-gray-800">último Producto</h5>
+                    <h5 className="m-0 font-weight-bold text-gray-800">Último Producto</h5>
                 </div>
                 <div className="card-body">
                     <div className="text-center">
